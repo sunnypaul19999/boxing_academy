84a4170671edce61129d0b2c3c43d849801a84b3
sunnypaul19999 <sunnypaul19999@gmail.com>
2022-02-22 15:48:29 +0530

    removed not req commented code from signinapi

--- src/server/SignInAPI/SignInAPI.js
+++ src/server/SignInAPI/SignInAPI.js
@@ -27,34 +27,3 @@ export default class SignInAPI {
     }
   }
 }
-
-/*axios
-.post("http://localhost:8080/user/signup", {
-  username: faker.internet.userName(),
-  password: faker.internet.password(),
-  email: faker.internet.email(),
-}).then((res) => {
-  let token = `${res.data.token}`.split('.');
-  let tokenPayload = atob(token[1]);
-  console.log(tokenPayload);
-  console.log('signup successfull');
-}).catch((err) => {
-  console.log(`login failed ${err['response'].status}`)
-});
-
-axios
-.post("http://localhost:8080/user/login", {
-  username: "Isai_Jones",
-  password: "qp90tsNXnAgTshJ",
-  email: "Kameron24@hotmail.com",
-}).then(
-  (res) => {
-    let token = `${res.data.token}`.split('.');
-    let tokenPayload = atob(token[1]);
-    console.log(tokenPayload);
-    console.log('login successfull');
-  }
-).catch((err) => {
-  console.log(`login failed ${err['response'].status}`)
-});
-});*/
