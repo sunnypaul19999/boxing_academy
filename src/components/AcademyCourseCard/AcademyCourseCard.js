ecc2737807231be863a48e0806406dd9f4c5f67d
sunnypaul19999 <sunnypaul19999@gmail.com>
2022-03-14 17:59:45 +0530

    changed cardAdminOnClick function to  cardAcademyAdminOnClickAction

--- src/components/AcademyCourseCard/AcademyCourseCard.js
+++ src/components/AcademyCourseCard/AcademyCourseCard.js
@@ -6,7 +6,7 @@ import CardToolbar from 'components/AcademyCourseCard/CardMakingTools/CardToolba
 import CardInfoOne from 'components/AcademyCourseCard/CardMakingTools/CardInfoOne.js';
 import CardInfoTwo from 'components/AcademyCourseCard/CardMakingTools/CardInfoTwo.js';
 import { cardUserOnClickAction } from 'components/AcademyCourseCard/Actions/User/Card/cardUserOnClickAction';
-import { cardAdminOnClickAction } from 'components/AcademyCourseCard/Actions/Admin/Card/cardAdminOnClick';
+import { cardAcademyAdminOnClickAction } from 'components/AcademyCourseCard/Actions/Admin/Card/cardAdminOnClick';
 import { adminEditCardEvent } from './Actions/Admin/Card/cardAdminOnEdit';
 import { adminDeleteCardEvent } from './Actions/Admin/Card/cardAdminOnDelete';
 import { cardUserOnEnrollCourseAction } from './Actions/User/Card/cardUserOnEnrollAction';
@@ -127,7 +127,7 @@ export default function AcademyCourseCard(props) {
     let cardOnClickHandler = (event) => {
         if (state.cardOf === 'academy') {
             if (state.authorityType === 'admin') {
-                cardAdminOnClickAction(event, state, nav);
+                cardAcademyAdminOnClickAction(event, state, nav);
             }
             if (state.authorityType === 'user') {
                 cardUserOnClickAction(event, state, nav);
--- src/components/AcademyCourseCard/Actions/Admin/Card/cardAdminOnClick.js
+++ src/components/AcademyCourseCard/Actions/Admin/Card/cardAdminOnClick.js
@@ -1,7 +1,7 @@
-export function cardAdminOnClickAction(event, state, nav) {
+export function cardAcademyAdminOnClickAction(event, state, nav) {
     event.preventDefault();
     event.stopPropagation();
-    
+
     let cardProp = state.cardProp;
 
     nav(`${cardProp.id}/courses`);
