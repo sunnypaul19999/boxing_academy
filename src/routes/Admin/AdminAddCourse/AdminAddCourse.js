b40a71e7851f72e8cd1022474acbf8fb370d93b7
Helios <sunnypaul19999@gmail.com>
2022-04-06 00:47:12 +0530

    updated academy course form regex

--- src/routes/Admin/AdminAddCourse/AdminAddCourse.js
+++ src/routes/Admin/AdminAddCourse/AdminAddCourse.js
@@ -1,48 +1,72 @@
-import AcademyCourseDetailsForm from "components/Forms/LayoutTwo/AcademyCourseDetailsForm";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import CardContainerNotifier from "store/CardContainerNotifier/CardContainerNotifier";
+
+import Formxvi from "components/Form/Formxvi";
+import FxInput from "components/Form/FxInput";
+import FxTextarea from "components/Form/FxTextarea";
 import { academyCourseDetailsFormFormat } from "components/Forms/LayoutTwo/academyCourseDetailsFormFormat.js";
+import CourseAPI from "server/CourseAPI/CourseAPI";
+
+
 
 
 export default function AdminAddCourse(props) {
 
-    let formInputFormat = (type) => {
-        return (academyCourseDetailsFormFormat.course.add.input[type]);
-    }
+    let navigate = useNavigate();
 
-    let formButtonFormat = (type) => {
-        return (academyCourseDetailsFormFormat.course.add.button[type]);
-    }
+    let courseFormInput = academyCourseDetailsFormFormat.course.add.input;
 
-    let onFormSubmit = (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        let sformat = serverFormat(new FormData(event.target));
-        console.log(sformat);
+    useEffect(() => {
+        console.log('AdminAddCourse rendered');
+    });
+
+
+    let onFormSubmit = (formState) => {
         console.log('AdminAddCourse submitted');
+        addCourse(serverFormat(formState));
     }
 
-    let serverFormat = (formData) => {
-        let formFormat = academyCourseDetailsFormFormat.course.add.input;
+    let serverFormat = (formState) => {
+        console.log(formState);
         return {
-            "courseName": formData.get(formFormat.course_name.name),
-            "courseDesc": formData.get(formFormat.course_description.name),
-            "courseDuration": formData.get(formFormat.course_duration.name),
-            "courseTimings": formData.get(formFormat.course_timing),
-            //"maxCourseStudents": formData.get(formFormat.course_total_students),
+            "courseName": formState[courseFormInput.academy_name.id].value,
+            "courseDuration": formState[courseFormInput.academy_location.id].value,
+            "courseCost": formState[courseFormInput.academy_contact_number.id].value,
+            "courseTimings": formState[courseFormInput.academy_email.id].value,
+            "courseDesc": formState[courseFormInput.academy_description.id].value,
         };
     }
 
+    let addCourse = async (sformat) => {
+        let response = await CourseAPI.add(sformat);
+        console.log(response.message);
+        CardContainerNotifier.update();
+        navigate(-1);
+    }
+
+
+
     return (
-        <AcademyCourseDetailsForm
-            submitButton={
-                {
-                    ...formButtonFormat('add_course'),
-                    onSubmit: onFormSubmit,
-                }}>
-            <input {...formInputFormat('course_name')} />
-            <input {...formInputFormat('course_duration')} />
-            <input {...formInputFormat('course_timing')} />
-            <input {...formInputFormat('course_total_students')} />
-            <textarea {...formInputFormat('course_description')}></textarea>
-        </AcademyCourseDetailsForm>
+        <Formxvi
+            id='addCourse'
+            title='Add Course'
+            onFormSubmit={onFormSubmit}>
+            <FxInput {...courseFormInput.academy_name} />
+            <FxInput {...courseFormInput.academy_contact_number} />
+            <FxInput {...courseFormInput.academy_image_url} />
+            <FxInput {...courseFormInput.academy_location} />
+            <FxInput {...courseFormInput.academy_email} />
+            <FxTextarea {...courseFormInput.academy_description} />
+        </Formxvi>
     );
+
 }
+
+/*id = { courseFormInput.academy_contact_number.id }
+name = { courseFormInput.academy_contact_number.name }
+label = { courseFormInput.academy_contact_number.placeholder }
+regex = '^[0-9]{10}$'
+errorMsg = 'Please enter valid phone number'
+required*/
